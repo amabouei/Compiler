73319239809295d366800ba943c519797c79b953
benyamxn <bengbenji@gmail.com>
2019-06-29 14:57:02 +0430

    defined some semantic token types

--- src/semantic/SemanticTokenType.java
+++ src/semantic/SemanticTokenType.java
@@ -1,9 +1,14 @@
 package semantic;
 
 public enum SemanticTokenType {
-    SOME_TOKEN;
+    NOT_VOID, CREATE_VAR, ASSIGN_NAME, INT, FIND_ID, VOID, IS_NUMERIC, ID_IS_NUMERIC, NUMERIC, NEW_SCOPE, END_OF_SCOPE, BREAK, CONTINUE,
+    WHILE, END_OF_WHILE, SWITCH, END_OF_SWITCH, BEGIN_ARGS, END_ARGS, ARG, NUM;
 
     public static SemanticTokenType getSemanticToken (String name) {
-        return SemanticTokenType.SOME_TOKEN;
+        for (SemanticTokenType token : SemanticTokenType.values()) {
+            if (token.name().equals(name.toUpperCase()))
+                return token;
+        }
+        return null;
     }
 }
